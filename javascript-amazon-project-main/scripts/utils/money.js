e85d5c9d4c924ef7754989e73fc69babd46d56da
Ramachandran <svel936@gmail.com>
2026-04-25 10:04:53 +0530

    Fix price formatting and radio Selectors

---
+++ javascript-amazon-project-main/scripts/utils/money.js
@@ -0,0 +1,20 @@
+
+
+export function formatCurreny(priceCents){
+  return "$" + (priceCents/100).toFixed(2)
+  
+}
+
+console.log(formatCurreny(12500000.00))
+
+
+
+// function formatCurrency(priceCents, currency = 'USD', locale = 'en-US') {
+//   return new Intl.NumberFormat(locale, {
+//     style: 'currency',
+//     currency: currency
+//   }).format(priceCents / 100);
+// }
+
+// console.log(formatCurrency(125000)); // $3.00
+// console.log(formatCurrency(300, 'INR', 'en-IN')); // ₹3.00
