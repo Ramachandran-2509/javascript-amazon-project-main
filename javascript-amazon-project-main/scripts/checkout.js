4f4b3c3d2ff3558ac1cae7dd78d01de4d1cce200
Ramachandran <svel936@gmail.com>
2026-05-09 08:19:50 +0530

    Create class version of cart

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderorderSummary} from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
+// import '../data/cart-oop.js';
 
 renderorderSummary()
 renderPaymentSummary()
